b79faff23c48bfe81dfca5ba10fce1e9a1a5f119
Robert Pohl <robert@sugarcubesolutions.com>
2014-09-15 09:56:44 +0200

    refactoring + support for One-page checkout

--- payment.php
+++ payment.php
@@ -2,9 +2,9 @@
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../header.php');
 include(dirname(__FILE__).'/mondidopay.php');
- 
-if (!$cookie->isLogged())
-    Tools::redirect('authentication.php?back=order.php');
+
+//if (!$cookie->isLogged())
+//    Tools::redirect('authentication.php?back=order.php');
 
 
 
--- validation.php
+++ validation.php
@@ -1,32 +1,32 @@
 <?php
 /**
-*  $Id$
-*  mondidopayment Module
-* 
-* Copyright @copyright 2014 3o-BPO
-* 
-* @category Payment
-* @version 1.0
-* @copyright 01.06.2014, 3o-BPO
-* @author Jeeky Vincent Mojica, <www.3obpo.com> 
-* @link
-* @license
-* 
-* Description:
-* 
-* Payment module mondidopay
-* 
-* --
-* 
-* This source file is subject to the Open Software License (OSL 3.0)
-* that is bundled with this package in the file LICENSE.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@touchdesign.de so we can send you a copy immediately.
-* 
-*/
+ *  $Id$
+ *  mondidopayment Module
+ *
+ * Copyright @copyright 2014 3o-BPO
+ *
+ * @category Payment
+ * @version 1.0
+ * @copyright 01.06.2014, 3o-BPO
+ * @author Jeeky Vincent Mojica, <www.3obpo.com>
+ * @link
+ * @license
+ *
+ * Description:
+ *
+ * Payment module mondidopay
+ *
+ * --
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@touchdesign.de so we can send you a copy immediately.
+ *
+ */
 
 require dirname(__FILE__).'/../../config/config.inc.php';
 include(dirname(__FILE__).'/../../header.php');
@@ -63,32 +63,32 @@ function httpAuth2(){
 	}
 */
 
-if (count($transaction_id)>0){
-	$merchantID = Configuration::get('MONDIDO_MERCHANTID');
-		$password =Configuration::get('MONDIDO_PASSWORD');
-		$remoteurl = 'https://api.mondido.com/v1/transactions/'. $transaction_id;
-		
-		$opts = array('http' => array('method' => "GET",
-			'header' => "Authorization: Basic " . base64_encode("$merchantID:$password")
-		));
-		
-		$context = stream_context_create($opts);
-		
-		$file = file_get_contents($remoteurl, false, $context);
-		 $data = (array) json_decode($file, true);
-	$order = new Order($mondidopay->currentOrder);
-	$payments = $order->getOrderPaymentCollection();
-	$payments[0]->transaction_id = $transaction_id;
-	$payments[0]-> card_number = $data['card_number'];
-	$payments[0]-> card_holder = $data['card_holder'];
-	$payments[0]-> card_brand = $data['card_type'];
-	$payments[0]->update();
+if (isset($_GET['transaction_id'])){
+    $merchantID = Configuration::get('MONDIDO_MERCHANTID');
+    $password =Configuration::get('MONDIDO_PASSWORD');
+    $remoteurl = 'https://api.mondido.com/v1/transactions/'. $transaction_id;
+
+    $opts = array('http' => array('method' => "GET",
+        'header' => "Authorization: Basic " . base64_encode("$merchantID:$password")
+    ));
+
+    $context = stream_context_create($opts);
+
+    $file = file_get_contents($remoteurl, false, $context);
+    $data = (array) json_decode($file, true);
+    $order = new Order($mondidopay->currentOrder);
+    $payments = $order->getOrderPaymentCollection();
+    $payments[0]->transaction_id = $transaction_id;
+    $payments[0]-> card_number = $data['card_number'];
+    $payments[0]-> card_holder = $data['card_holder'];
+    $payments[0]-> card_brand = $data['card_type'];
+    $payments[0]->update();
 }
 /*'card_holder' =>  $file['card_holder'],
 				'card_number' =>  $file['card_number'],
 				'card_type' =>  $file['card_type'],
 */
-	
-Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?payment_ref='.$payment_ref.'&id_module='.$mondidopay->id.'&id_order='.$mondidopay->currentOrder.'&key='.$order->secure_key.'&transaction_id='.$transaction_id.'&hash='.$hash);
+
+Tools::redirectLink(_PS_BASE_URL_ . __PS_BASE_URI__.'index.php?controller=order-confirmation&id_cart='.(int)$cart->id.'&payment_ref='.$payment_ref.'&id_module='.$mondidopay->id.'&id_order='.$mondidopay->currentOrder.'&key='.$order->secure_key.'&transaction_id='.$transaction_id.'&hash='.$hash);
 
 ?>
