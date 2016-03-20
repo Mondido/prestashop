de86b977b487031afd97b845d8aaa33f450744a6
Robert Pohl <robert@sugarcubesolutions.com>
2016-03-20 15:58:15 +0100

    fixed templates files

---
+++ views/templates/admin/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*    $Id$ mondidopayment Module
+*
+*    Copyright @copyright 2016 Mondido
+*
+*    @category  Payment
+*    @version   1.4.0
+*    @author    Mondido
+*    @copyright 2016 Mondido
+*    @link      https://www.mondido.com
+*    @license   MIT
+*
+*   Description:
+*   Payment module mondidopay
+*/
+header('Expires: Fri, 30 May 2014 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../../../');
+
+?>
